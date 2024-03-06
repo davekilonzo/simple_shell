f49687f20a4bfe04b07e8c0fd9171cb9eba7b189
davekilonzo <davidkilonzo1998@gmail.com>
2024-03-06 03:43:41 -0800

    task0

---
+++ betty.c
@@ -0,0 +1,17 @@
+#include "header.h"
+
+/**
+ * add_numbers - Adds two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Returns the sum
+ */
+
+int add_numbers(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+/* Function prototypes */
+int add_numbers(int a, int b);
+
+#endif /* HEADER_H */
+
